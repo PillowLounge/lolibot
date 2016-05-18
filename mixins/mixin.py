527e669587500f234309820aea0a726d2bc59b8c
PillowLounge <binmagus@gmail.com>
2016-05-18 21:29:40 +0200

    refactor callmixins

--- mixins/mixin.py
+++ mixins/mixin.py
@@ -1,4 +1,7 @@
 
+#intended usage
+#class NewMixin(mixin): #treat as metaclass
+#class Implementation(NewMixin): #treat normally
 
 def split_on(iterable, predicate):
 	a = []
@@ -34,22 +37,27 @@ class mixin(type):
 	mixins and for normal use'''
 
 	def __new__(cls, name, bases, attrs):
-		print('mixin.__new__('+ ', '.join((
-			repr(cls), repr(name), repr(bases), repr(attrs)
-		)) +')')
+		print('new mixin:'+ str(name), str(attrs.keys()))
 		mixins, bases = split_on(bases,
 			lambda x: tryattr(x, 'metaclass') == mixin)
 		attrs['mixins'] = mixins
-		print('mixins:'+ ('' if mixins else ' None'))
-		for m in mixins:
-			print('\t'+ m)
-			trying(m._metanew_, (cls, name, bases, attrs), AttributeError)
-		for m in mixins:
-			trying(m._metainit_(cls, name, bases, attrs), AttributeError)
+		#print('mixins:'+ ('' if mixins else ' None'))
+		mixin.callmixins(mixins, '_metanew_', (cls, name, bases, attrs))
+		mixin.callmixins(mixins, '_metainit_', (cls, name, bases, attrs))
 		if not bases: attrs['metaclass'] = mixin #TODO: examine
 
-		return type(name, tuple(bases), attrs)
+		return type.__new__(cls, name, tuple(bases), attrs)
 
+	@staticmethod
+	def callmixins(mixins, attrname, args):
+		for m in mixins:
+			print('\t'+ repr(m))
+			builder = tryattr(m, attrname)
+			if builder:
+				try: builder(*args)
+				except Exception as err:
+					print('ERROR: '+ name +'.'+ attrname +' failed execution')
+					print(err)
 
 if __name__ == '__main__':
 	class Test(metaclass=mixin):
